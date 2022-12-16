2b1b013ab0df35f39d05da4c7685b6565be28dfe
Tim Cook <53342498+FreeDivers@users.noreply.github.com>
2022-12-16 23:52:33 +0800

    Update menu.js
    Fix the problem of Menu being uppercase and lowercase at one time

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,7 +3,7 @@ function toggleMenu() {
     var menuButton = document.getElementById("menu-btn");  
     if(menuList.classList.contains("active")){
       menuList.classList.remove("active");
-      menuButton.innerHTML = "MENU";
+      menuButton.innerHTML = "Menu";
     }else{
       menuList.classList.add("active");
       menuButton.innerHTML = "<div class=\"icon arrow-up\"> </div>";
